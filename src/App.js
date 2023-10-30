9c1df13ead63c6ff3dd2784d3a372282645eda88
MullaneyDev <matt.mullaney@live.co.uk>
2023-10-30 17:21:57 +0000

    addition of links to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import "./App.css"
+import "./App.css";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return <div className="App">
-
+    <nav>
+      <Link />
+      <Link />
+      <Link />
+    </nav>
   </div>;
 }
 
