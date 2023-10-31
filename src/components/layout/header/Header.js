7c151bf7a07c28b8f42ed2b549370ba9bf420673
MullaneyDev <matt.mullaney@live.co.uk>
2023-10-31 10:28:10 +0000

    CSS for not logged in page

--- src/components/layout/header/Header.js
+++ src/components/layout/header/Header.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import "./Header.css";
 
-const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+const Header = ({ user, loggedIn }) => {
+  if (!loggedIn) {
+    return (
+      <div className="header">
+        <h1>Promptelex</h1>
+        <h3>Please log in</h3>
+      </div>
+    );
+  } else {
+    return (
+      <div className="header">
+        <h1>Promptelex</h1>
+      </div>
+    );
+  }
+};
 
 export default Header
