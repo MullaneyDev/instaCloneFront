b39dcfcb6d58611ed1b9a6900540fb40495252e0
Nathan Stringer <nathstringer@gmail.com>
2023-11-02 10:56:51 +0000

    creation of branch to start bringing in photos from db

--- src/components/cards/Card/Card.js
+++ src/components/cards/Card/Card.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Card.css";
 
 const Card = ({ user }) => {
+  console.log(user);
   return (
     <div className="card">
       <p className="username">{user.username}</p>
